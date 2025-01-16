20d9769477aa1d117b5e5373c7a8d1616862ba78
Matheus Rogato <matheusrogato@gmail.com>
2025-01-16 18:06:11 -0300

    feat:enable cors

--- prisma/migrations/20241105221651_add_roles/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Admin" ADD COLUMN     "role" TEXT NOT NULL DEFAULT 'admin';
-
--- AlterTable
-ALTER TABLE "Supplier" ADD COLUMN     "role" TEXT NOT NULL DEFAULT 'supplier';
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "role" TEXT NOT NULL DEFAULT 'user';
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   const configSwagger = new DocumentBuilder()
     .setTitle('Turismo Project API')
     .setDescription('API para o projeto conheça seu destino')
